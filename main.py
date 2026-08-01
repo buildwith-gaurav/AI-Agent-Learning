eb326c60fe258edf4a989b8191e0a66872c30c91
buildwith-gaurav <gk3819372@gmail.com>
2026-08-01 19:34:01 +0530

    day 02 : added about endpoint

--- main.py
+++ main.py
@@ -5,3 +5,12 @@ app = FastAPI()
 @app.get("/")
 def home():
     return{"message: Hello AI Agent Developer "}
+
+@app.get("/about")
+def about():
+    return {
+        {"message: AI Agent Learning"},
+        {"day":2},
+        {"developer":"buildwith-gaurav"}
+    }
+ 
