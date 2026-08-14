1c4389a70f145fffecc287a7e38f294765e25eda
buildwith-gaurav <gk3819372@gmail.com>
2026-08-14 16:08:00 +0530

    Day 11: Added memory with tool calling

--- services.py
+++ services.py
@@ -2,25 +2,19 @@ from google import genai
 from google.genai import types
 
 from config import GEMINI_API_KEY
-from tools import multiply,add
+from tools import add , multiply
 
+client = genai.Client(api_key = "AGEMINI_API_KEY")
 
-client = genai.Client(api_key="GEMINI_API_KEY")
-
-
-def ask_gemini(prompt: str):
-
-    response = client.models.generate_content(
-        model="gemini-3.6-flash",
-        contents=prompt,
-        config=types.GenerateContentConfig(
-            tools=[multiply,add],
-            automatic_function_calling=types.AutomaticFunctionCallingConfig(
-                disable=False
-            )
+chat = client.chats.create(
+    model = "gemini-3.6-flash",
+    config = types.GenerateContentConfig(
+         tools=[add, multiply],
+        automatic_function_calling=types.AutomaticFunctionCallingConfig(
+            disable=False
         )
     )
-
-    print("TOOL HISTORY:", response.automatic_function_calling_history)
-
+)
+def ask_gemini(prompt:str):
+    response = chat.send_message(prompt)
     return response.text
