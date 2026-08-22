c5061f3b75149203b5f451014fec1aaa15f05031
buildwith-gaurav <gk3819372@gmail.com>
2026-08-22 12:06:01 +0530

    Day 17: Added FastAPI response model

--- main.py
+++ main.py
@@ -9,12 +9,16 @@ app = FastAPI()
 class PromptRequest(BaseModel):
     prompt: str
 
+class AgentResponse(BaseModel):
+    city: str | None = None
+    temperature: float | None = None
+    calculation: int | None = None
+    final_result: int | None = None    
 
-@app.post("/chat")
+
+@app.post("/chat", response_model=AgentResponse)
 def chat(user: PromptRequest):
 
     result = run_prompt(user.prompt)
 
-    return {
-        "response": str(result)
-    }
+    return result
